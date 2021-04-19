19fff539a9b2846d437254c24c08b510a3b04d05
Letta19 <82350726+Letta19@users.noreply.github.com>
2021-04-20 00:11:17 +0700

    Add files via upload

---
+++ HabitTracker/LoginForm/Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace LoginForm
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Asus\Documents\MySideHabit.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT (*) FROM LoginTbl WHERE Uname= '" + tbUsername.Text + "' AND uPass= '" + tbPassword.Text + "'", con);
+
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                //new main().Show();//
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password");
+
+            }
+        }
+    }
+}
