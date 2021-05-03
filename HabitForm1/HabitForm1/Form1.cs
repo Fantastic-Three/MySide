981d047d51456326cce70394f97d1b7588da8b99
Letta19 <82350726+Letta19@users.noreply.github.com>
2021-05-04 02:10:44 +0700

    Add files via upload

---
+++ HabitForm1/HabitForm1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        string Opsi;
+        SqlConnection sqlcon = new SqlConnection(@"Data Source = DESKTOP-AGMG99M; Initial Catalog=datetime; Integrated Security=True; Pooling=False");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            sqlcon.Open();
+            SqlCommand cmd = sqlcon.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Masukkan nilai ke dalam tabel ('"+ tbNamaHabit.Text +"', '"+ tbKeterangan.Text +"', '"+ dateTimePicker1.Value.ToString() +"', '" + Opsi +"')";
+            cmd.ExecuteNonQuery(); 
+            sqlcon.Close();
+            MessageBox.Show("Data Habit Berhasil Disimpan");
+        }
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            Opsi = "Done";
+        }
+
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            Opsi = "Not Yet";
+        }
+    }
+}
---
+++ HabitForm1/HabitForm1/dbo.DaftarHabit.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [namahabit] NVARCHAR(50) NULL, 
+    [keterangan] NVARCHAR(50) NULL, 
+    [hdate] DATE NULL, 
+    [option] BIT NULL
+)
