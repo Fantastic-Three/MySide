61041e3f34485222bc8ea1a39c6c0ccc08b9868e
latifahd <latifahdewi27@gmail.com>
2021-06-12 21:05:43 +0700

    update

--- HabitForm1/HabitForm1/dbo.DaftarHabit.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [namahabit] NVARCHAR(50) NULL, 
-    [keterangan] NVARCHAR(50) NULL, 
-    [hdate] DATE NULL, 
-    [option] BIT NULL
-)
